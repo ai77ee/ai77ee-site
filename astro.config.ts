b0f871f949296a0e1007caf26d81623cfb9d68ce
ai77ee <12903667+ai77ee@users.noreply.github.com>
2025-01-31 18:56:50 -0600

    changed to astro-pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,8 @@ import spectre from './package/src';
 import { spectreDark } from './src/ec-theme';
 import vercel from '@astrojs/vercel';
 import preact from '@astrojs/preact';
-
+import pagefind from "astro-pagefind";
+import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ai77ee.vercel.app',
@@ -40,6 +41,7 @@ export default defineConfig({
       emitMetadata: false,
       lang: 'en',
     }
-  }), preact()],
- adapter: vercel()
+  }), preact(), pagefind()],
+ adapter: vercel(),
+ //adapter: node({ mode: 'standalone' }), // Use standalone adapter for local testing
 });
