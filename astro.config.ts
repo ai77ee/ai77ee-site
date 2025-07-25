c10f2cfd7a10775c3a8e807b4f75dd5b8a343f08
ai77ee <12903667+ai77ee@users.noreply.github.com>
2025-07-24 21:23:31 -0600

    Changed astro.config file

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,30 @@
+import { loadEnv } from "vite";
 import { defineConfig } from 'astro/config';
 
 import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import spectre from './package/src';
-import { spectreDark } from './src/ec-theme';
 import vercel from '@astrojs/vercel';
 import preact from '@astrojs/preact';
 import node from '@astrojs/node';
-import dotenv from 'dotenv';
+import spectre from './package/src';
+import { spectreDark } from './src/ec-theme';
 
-dotenv.config();
+const env = loadEnv(process.env.NODE_ENV!, process.cwd(), "PUBLIC_"); 
+
+
 const {
-  GISCUS_REPO,
-  GISCUS_REPO_ID,
-  GISCUS_CATEGORY,
-  GISCUS_CATEGORY_ID,
-  GISCUS_MAPPING,
-  GISCUS_STRICT,
-  GISCUS_REACTIONS_ENABLED,
-  GISCUS_EMIT_METADATA,
-  GISCUS_LANG
-} = process.env;
-
+  PUBLIC_GISCUS_REPO,
+  PUBLIC_GISCUS_REPO_ID,
+  PUBLIC_GISCUS_CATEGORY,
+  PUBLIC_GISCUS_CATEGORY_ID,
+  PUBLIC_GISCUS_MAPPING,
+  PUBLIC_GISCUS_STRICT,
+  PUBLIC_GISCUS_REACTIONS_ENABLED,
+  PUBLIC_GISCUS_EMIT_METADATA,
+  PUBLIC_GISCUS_LANG
+} = env; 
+const IS_VERCEL_ENV = process.env.VERCEL === '1';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ai77ee.vercel.app',
@@ -45,20 +47,22 @@ export default defineConfig({
       }
     },
     giscus: {
-      repository: GISCUS_REPO ?? '',
-      repositoryId:  GISCUS_REPO_ID ?? '',
-      category: GISCUS_CATEGORY ?? '',
-      categoryId: GISCUS_CATEGORY_ID ?? '',
-      mapping:  GISCUS_MAPPING as any,
-      strict: GISCUS_STRICT === "true",
-      reactionsEnabled: GISCUS_REACTIONS_ENABLED === "true",
-      emitMetadata:  GISCUS_EMIT_METADATA === "true",
-      lang: GISCUS_LANG ?? '',
+      repository: PUBLIC_GISCUS_REPO!, 
+      repositoryId: PUBLIC_GISCUS_REPO_ID!,
+      category: PUBLIC_GISCUS_CATEGORY!,
+      categoryId: PUBLIC_GISCUS_CATEGORY_ID!,
+      mapping: PUBLIC_GISCUS_MAPPING as "pathname",
+      strict: PUBLIC_GISCUS_STRICT === "true",
+      reactionsEnabled: PUBLIC_GISCUS_REACTIONS_ENABLED === "true",
+      emitMetadata: PUBLIC_GISCUS_EMIT_METADATA === "true",
+      lang: PUBLIC_GISCUS_LANG!,
     }
   }), preact()],
 
-  adapter: process.env.ENVIRONMENT === 'vercel' ? vercel({
-    imageService: true,
-    devImageService: 'squoosh',
-  }) : node({ mode: 'standalone' }),
+  adapter: process.env.VERCEL === '1'
+    ? vercel({
+        imageService: true,
+        devImageService: 'squoosh',
+      })
+    : node({ mode: 'standalone' }),
 });
